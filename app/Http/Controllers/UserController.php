27cb1265270b3d26a44df9b90fd123528384165d
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-02-25 18:56:41 +0700

    Pertemuan 3_Praktikum 6_langkah 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        // coba alses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
 }
