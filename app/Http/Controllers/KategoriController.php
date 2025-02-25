a0a415b3a964d2ce11d92396f37a67d97e93c317
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-02-25 18:16:43 +0700

    Pertemuan 3_Praktikum 5_langkah 8 dan 9

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -17,7 +17,13 @@ class KategoriController extends Controller
         // DB::table('m_kategori')->insert($data);
         // return 'Data berhasil ditambahkan';
 
-        $row = DB::table('m_kategori')->where('kategori_kode', 'K06')->update(['kategori_nama' => 'Buku Tulis']);
-        return 'Data berhasil diupdate. Jumlah data yang diupdate: ' . $row . ' baris';
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'K06')->update(['kategori_nama' => 'Buku Tulis']);
+        // return 'Data berhasil diupdate. Jumlah data yang diupdate: ' . $row . ' baris';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'K06')->delete();
+        // return 'Data berhasil dihapus. Jumlah data yang dihapus: ' . $row . ' baris';
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
     }
 }
