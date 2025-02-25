47b8d6d44a14920eb3d1ddc9f70812cb70a9c71b
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-02-25 19:22:30 +0700

    Pertemuan 3_Praktikum 6_langkah 8

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
+        // tambah data user dengn Eloquent Model
+        $data = [
+            'username' => 'customer 1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
+        UserModel::create($data); // insert data ke tabel m_user
+        
         // coba alses model UserModel
         $user = UserModel::all(); // ambil semua data dari tabel m_user
         return view('user', ['user' => $user]);
