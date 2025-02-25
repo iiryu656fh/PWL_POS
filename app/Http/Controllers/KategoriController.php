1a48e46f37b539d359ef076df4a9695bc54ff6b6
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-02-25 18:08:43 +0700

    Pertemuan 3_Praktikum 5_langkah 5

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -9,12 +9,15 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        $data = [
-            'kategori_kode' => 'K06',
-            'kategori_nama' => 'Buku',
-            'created_at' => now()
-        ];
-        DB::table('m_kategori')->insert($data);
-        return 'Data berhasil ditambahkan';
+        // $data = [
+        //     'kategori_kode' => 'K06',
+        //     'kategori_nama' => 'Buku',
+        //     'created_at' => now()
+        // ];
+        // DB::table('m_kategori')->insert($data);
+        // return 'Data berhasil ditambahkan';
+
+        $row = DB::table('m_kategori')->where('kategori_kode', 'K06')->update(['kategori_nama' => 'Buku Tulis']);
+        return 'Data berhasil diupdate. Jumlah data yang diupdate: ' . $row . ' baris';
     }
 }
