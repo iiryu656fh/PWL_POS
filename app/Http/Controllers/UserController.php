5b05d0d9aee2eddd0d3424f629f9c0b57a3a476d
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-03 18:09:10 +0700

    Pertemuan 4_Praktikum 1_langkah 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,8 +17,8 @@ class UserController extends Controller
         // UserModel::where('username', 'customer 1')->update($user); //update data user
         $data = [
             'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
             'password' => Hash::make('12345'),
         ];
         UserModel::create($data); // insert data user
