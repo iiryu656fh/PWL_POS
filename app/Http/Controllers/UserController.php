708ffd03d9cf8c2964a8148bf7869914a0a5b529
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-03 18:03:18 +0700

    Pertemuan 4_Praktikum 1_langkah 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,14 +10,21 @@ class UserController extends Controller
 {
     public function index()
     {
-        // tambah data user dengn Eloquent Model
-        $user = [
-            'nama' => 'Pelanggan Pertama',
+        // // tambah data user dengn Eloquent Model
+        // $user = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer 1')->update($user); //update data user
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_dua',
+            'nama' => 'Manager 2',
+            'password' => Hash::make('12345'),
         ];
-        UserModel::where('username', 'customer 1')->update($user); //update data user
-        
+        UserModel::create($data); // insert data user
         // coba akses model UserModel
+
         $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['user' => $user]);
+        return view('user', ['data' => $user]);
     }
 }
