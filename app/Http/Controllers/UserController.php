3b54b01e5f199bc378dc45c133e128f9c30a4ab5
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-03 19:32:46 +0700

    Pertemuan 4_Praktikum 2.5_langkah 1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,30 +10,29 @@ class UserController extends Controller
 {
     public function index()
     {
-        // // tambah data user dengn Eloquent Model
-        // $user = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', 'customer 1')->update($user); //update data user
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data); // insert data user
-        // // coba akses model UserModel
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
 
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        $user->username = 'manager56';
+
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean(['nama', 'username']); // false
+
         $user->save();
-        
-        return view('user', ['data' => $user]);
+
+        $user->isDirty(); // false
+        $user->isClean(); // true
+        dd($user->isDirty());
     }
 }
