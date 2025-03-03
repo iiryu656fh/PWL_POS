78869fd16f4b0fa9c76ec338c19e6750ef9ee051
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-03 18:26:29 +0700

    Pertemuan 4_Praktikum 2.1_langkah 8

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,7 +24,9 @@ class UserController extends Controller
         // UserModel::create($data); // insert data user
         // // coba akses model UserModel
 
-        $user = UserModel::firstwhere('level_id', 1); 
+        $user = UserModel::findOr(1, ['username', 'nama'], function() {
+            abort(404);
+        }); 
         return view('user', ['data' => $user]);
     }
 }
