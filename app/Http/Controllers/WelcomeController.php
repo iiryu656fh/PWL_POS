1ce41c437a528a16404fa5bc8d59e1020e726235
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-09 20:15:59 +0700

    Pertemuan 5 - Praktikum 2 - Langkah 1 update

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    //
+    public function index(){
+        $breadcrumb = (object) [
+            'title' => 'Selamat Datang',
+            'list' => ['Home', 'Welcome']
+        ];
+
+        $acttiveMenu = 'dashboard';
+        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $acttiveMenu]);
+    }
 }
