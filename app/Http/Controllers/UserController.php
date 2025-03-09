d431e532fbc8741f6e151e3084231675f7d93ace
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-09 20:48:31 +0700

    Pertemuan 5 - Praktikum 3 - Langkah 4 update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\Hash;
+use App\Models\LevelModel;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
