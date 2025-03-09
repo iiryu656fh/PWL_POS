60f15888a913316cc655e2e9f173a1a11b4df1e5
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-09 20:41:54 +0700

    Pertemuan 5 - Praktikum 3 - Langkah 2-3 : library Yajra-datatables dan mengubah router

--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,36 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
+use App\Models\User;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/level', [LevelController::class, 'index']);
 
-Route::get('/kategori', [KategoriController::class, 'index']);
+// Route::get('/kategori', [KategoriController::class, 'index']);
 
-Route::get('/user', [UserController::class, 'index']);
+// Route::get('/user', [UserController::class, 'index']);
 
-Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
 
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']) ->name('user.ubah_simpan');
+// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']) ->name('user.ubah_simpan');
 
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'level'], function(){
+    Route::get('/', [UserController::class, 'index']); // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']); // menampilkan data user dalam bentuk json untuk datables
+    Route::get('/create', [UserController::class, 'create']); // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']); // menyimpan data user baru
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']); // menyimpan data user yang diubah
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
