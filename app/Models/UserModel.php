70840cb182a72ffd13a9039da61f9431e70932f2
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-23 13:53:35 +0700

    Pertemuan 7 - Praktikum 2 : Implementasi Authorization di Laravel dengan Middleware - langkah 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -33,4 +33,18 @@ class UserModel extends Model
     public function level(): BelongsTo {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+
+    /**
+     * Mendapatkan nama role
+     */
+    public function getRoleName(): string {
+        return $this->level->level_name;
+    }
+
+    /**
+     * Cek apakah user memiliki role tertentu
+     */
+    public function hasRole(string $role): bool {
+        return $this->level->level_kode == $role;
+    }
 }
