25ac5f0b3740a6f463f3eadad1ed15aa009b553c
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-03-26 03:11:39 +0700

    Pertemuan 7 - Praktikum 3 : Implementasi Multi-Level Authorization di Laravel dengan Middleware - langkah  2

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -13,14 +13,14 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $role = ''): Response
+    public function handle(Request $request, Closure $next, ...$roles): Response
     {
-        $user = $request->user();  // ambil data user yg login
+        $user_role = $request->user()->getRole();  // ambil data user yg login
                                     //  fungsi user() diambil dari USerModel.php
-        if($user->hasRole($role)){ // cek apakah user memiliki role yang diinginkan
+        if(in_array($user_role, $roles)){ // cek apakah user memiliki role yang diinginkan
             return $next($request);
         }
         // Jika tiak punya role, maka tampilkan error 403
-        abort(402, 'Forbidden. Kamu tidak punya akses ke halaman ini');
+        abort(403, 'Forbidden. Kamu tidak punya akses ke halaman ini');
     }
 }
