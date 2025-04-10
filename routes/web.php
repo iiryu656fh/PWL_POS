a9d1509a7f66d5cf8f058b0b716ac31df8dbc50e
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-04-10 16:22:32 +0700

    Pertemuan 8 - Praktikum 3: Implementasi Export PDF dengan dompdf - Langkah 3

--- routes/web.php
+++ routes/web.php
@@ -109,6 +109,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/import', [BarangController::class, 'import']); // ajax form uplaod excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
             Route::get('/export_excel', [BarangController::class, 'export_excel']); // ajax export excel
+            Route::get('/export_pdf', [BarangController::class, 'export_pdf']); // ajax export pdf
         });
     });
 
