22a6e7e82e6d25c7735680ca3c2e503662a89246
iiryu656fh <khoirotunnisa2507@gmail.com>
2025-05-02 21:11:36 +0700

    Pertemuan 9 - Praktikum 1: Membuat RESTful API Register - langkah 7-13

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|max:255',
+            'nama' => 'required|string|max:255',
+            'password' => 'required|string|min:5|confirmed',
+            'level_id' => 'required|integer|exists:m_level,level_id',
+        ]);
+
+        //if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create user
+        $user = UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => bcrypt($request->password),
+            'level_id' => $request->level_id,
+        ]);
+
+        //return response JSON user is created
+        if($user) {
+            return response()->json([
+                'message' => true,
+                'user' => $user
+            ], 201);
+        }
+
+        //return JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
+}
